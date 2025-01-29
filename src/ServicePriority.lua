a9c4cb135e856eca3bb3abc43928571804e12199
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2025-01-29 19:49:53 +0100

    Improve ServicePriority table type

--- src/ServicePriority.lua
+++ src/ServicePriority.lua
@@ -1,6 +1,6 @@
 ---@alias ServicePriority 0 | 1 | 2 | 3 | 4
 
----@type table<string, ServicePriority>
+---@type {Lowest: 0, Low: 1, Normal: 2, High: 3, Highest: 4}
 local ServicePriority = {
     Lowest = 0,
     Low = 1,
