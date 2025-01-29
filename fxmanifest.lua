5195d9776b3dbe7819daed96cd05ea6b0d55ac10
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2025-01-29 15:02:46 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+-- FX information
+fx_version "cerulean"
+game "common"
+lua54 "yes"
+use_experimental_fxv2_oal "yes"
+
+-- Resource information
+name "r3_servicesmanager"
+version "0.1.0"
+description "Services manager for FXServer"
+author "r3ps4J"
+contact "discord.gg/bEWmBbg"
+
+-- Manifest
+shared_script "dist/main.lua"
