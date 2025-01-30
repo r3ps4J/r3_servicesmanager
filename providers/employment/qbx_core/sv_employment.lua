a5fa95db1b42d1928822cdf16f65dc816565d03a
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2025-01-30 22:12:26 +0100

    Create employment provider for qbox

---
+++ providers/employment/qbx_core/sv_employment.lua
@@ -0,0 +1,107 @@
+RegisterOnResourceStart("employment", function()
+    ---@type ServerEmploymentProvider
+    local employmentProvider
+
+    employmentProvider = {
+        getJob = function(jobName)
+            local qbxJob = exports.qbx_core:GetJob(jobName)
+
+            if qbxJob == nil then
+                return nil
+            end
+
+            local label = qbxJob.label
+            ---@type table<integer, JobGrade>
+            local grades = {}
+
+            for grade, qbxJobGrade in pairs(qbxJob.grades) do
+                grades[grade] = {
+                    name = qbxJob.name, -- qbox doesn't differentiate between grade name and grade label
+                    label = qbxJobGrade.name,
+                    salary = qbxJob.payment,
+                    isBoss = qbxJob.isboss ~= nil and qbxJob.isboss,
+                }
+            end
+
+            return {
+                name = jobName,
+                label = label,
+                grades = grades,
+            }
+        end,
+        jobExists = function(jobName, jobGrade)
+            local qbxJob = exports.qbox_core.GetJob(jobName)
+
+            if qbxJob == nil then
+                return false
+            end
+
+            if jobGrade ~= nil and qbxJob.grades[jobGrade] == nil then
+                return false
+            end
+
+            return true
+        end,
+        getOnlineJobCount = function(jobName)
+            local count, players = exports.qbx_core:GetDutyCountJob(jobName)
+            return count
+        end,
+        getPlayerJob = function(playerId)
+            local player = exports.qbx_core:GetPlayer(playerId)
+
+            if player == nil then
+                return nil
+            end
+
+            local playerJob = player.PlayerData.job
+
+            return {
+                name = playerJob.name,
+                label = playerJob.label,
+                grade = {
+                    rank = playerJob.grade.level,
+                    name = playerJob.grade.name, -- qbox doesn't differentiate between grade name and grade label
+                    label = playerJob.grade.name,
+                    salary = playerJob.payment,
+                    isBoss = playerJob.isboss,
+                },
+            }
+        end,
+        setPlayerJob = function(playerId, jobName, jobGrade)
+            if not employmentProvider.jobExists(jobName, jobGrade) then
+                return false
+            end
+
+            local player = exports.qbx_core:GetPlayer(playerId)
+
+            if player == nil then
+                return false
+            end
+
+            exports.qbx_core:SetJob(playerId, jobName, jobGrade)
+
+            return true
+        end,
+        playerHasJob = function(playerId, jobName, jobGrade)
+            local player = exports.qbx_core:GetPlayer(playerId)
+
+            if player == nil then
+                return false
+            end
+
+            local playerJob = player.PlayerData.job
+
+            if playerJob.name ~= jobName then
+                return false
+            end
+
+            if jobGrade ~= nil and playerJob.grade.level < jobGrade then
+                return false
+            end
+
+            return true
+        end,
+    }
+
+    return employmentProvider
+end, ServicePriority.Lowest, "qbx_core")
