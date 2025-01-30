0949489ecabc9a4ef9586064fe1b583853ac9a4f
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2025-01-30 12:04:08 +0100

    Add missing return type to  lua definition of ServerNotificationProvider.showNotification

--- api/notification/definitions/notification.lua
+++ api/notification/definitions/notification.lua
@@ -8,4 +8,4 @@
 ---@field showNotification fun(message: string, options: NotificationOptions): nil
 
 ---@class ServerNotificationProvider
----@field showNotification fun(playerId: integer, message: string, options: NotificationOptions)
+---@field showNotification fun(playerId: integer, message: string, options: NotificationOptions): nil
