7c794793cb002a3ff70b4f87ad9db5634a31cbe7
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2025-01-31 21:02:52 +0100

    Add reverse indexing of ServicePriority enum

--- src/ServicePriority.lua
+++ src/ServicePriority.lua
@@ -1,6 +1,6 @@
 ---@alias ServicePriority 0 | 1 | 2 | 3 | 4
 
----@type {Lowest: 0, Low: 1, Normal: 2, High: 3, Highest: 4}
+---@type { Lowest: 0, Low: 1, Normal: 2, High: 3, Highest: 4 } | { [0]: "Lowest", [1]: "Low", [2]: "Normal", [3]: "High", [4]: "Highest" }
 local ServicePriority = {
     Lowest = 0,
     Low = 1,
@@ -8,5 +8,16 @@ local ServicePriority = {
     High = 3,
     Highest = 4,
 }
+
+local reversedServicePriority = {}
+for name, value in pairs(ServicePriority) do
+    reversedServicePriority[value] = name
+end
+
+setmetatable(ServicePriority, {
+    __index = function(t, key)
+        return reversedServicePriority[key]
+    end,
+})
 
 return ServicePriority
