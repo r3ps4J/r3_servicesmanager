b9ddb787f04f30c13d41d2f9d8b53132fe6d728f
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2025-01-31 21:06:04 +0100

    Add logger to services manager

--- src/ServicesManager.lua
+++ src/ServicesManager.lua
@@ -1,10 +1,15 @@
 local RegisteredProvider = require "RegisteredProvider"
+local ServicePriority = require "ServicePriority"
+local Logger = require "Logger"
 
 ---The constructor for the ServicesManager class.
 ---@return ServicesManager
 local function ServicesManager()
     ---@class ServicesManager
     local servicesManager = {}
+
+    local logger = Logger()
+    local currentResourceName = GetCurrentResourceName()
 
     ---@type table<string, RegisteredProvider[]>
     local providerMap = {}
@@ -31,7 +36,7 @@ local function ServicesManager()
     ---@param priority ServicePriority
     ---@param resource string
     function servicesManager.register(service, provider, priority, resource)
-        local invokingResource = GetInvokingResource()
+        local invokingResource = GetInvokingResource() or currentResourceName
         local registeredProvider = RegisteredProvider(service, provider, priority, resource, invokingResource)
 
         if not providerMap[service] then
@@ -40,6 +45,15 @@ local function ServicesManager()
 
         table.insert(providerMap[service], registeredProvider)
         sortProvidersForService(service)
+
+        logger.info(
+            ("'%s' registered a provider for service '%s' with priority '%s'%s."):format(
+                invokingResource,
+                service,
+                ServicePriority[priority],
+                resource ~= invokingResource and (" for resource '%s'"):format(resource) or ""
+            )
+        )
 
         TriggerEvent("r3_servicesmanager:providerRegistered", registeredProvider)
     end
@@ -102,6 +116,7 @@ local function ServicesManager()
     ---Unregisters all providers registered by a resource.
     ---@param resource string
     function servicesManager.unregisterAllForResource(resource)
+        logger.debug(("Unregistering all providers registered for or by resource '%s'."):format(resource))
         for service, providers in pairs(providerMap) do
             local unregisteredProviders = {}
             for i, provider in ipairs(providers) do
