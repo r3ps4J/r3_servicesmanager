20b20220f7d6a4c9cc6bb00ff633c03cdf20ea7d
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2025-02-01 19:03:37 +0100

    Replace number with integer where needed in employment provider lua definition

--- api/employment/definitions/employment.lua
+++ api/employment/definitions/employment.lua
@@ -20,7 +20,7 @@
 ---@class ServerEmploymentProvider
 ---@field getJob fun(jobName: string): Job | nil
 ---@field jobExists fun(jobName: string, jobGrade?: integer): boolean
----@field getOnlineJobCount fun(jobName: string): number
----@field getPlayerJob fun(playerId: number): PlayerJob | nil
----@field setPlayerJob fun(playerId: number, jobName: string, jobGrade: number): boolean
----@field playerHasJob fun(playerId: number, jobName: string, jobGrade: number): boolean
+---@field getOnlineJobCount fun(jobName: string): integer
+---@field getPlayerJob fun(playerId: integer): PlayerJob | nil
+---@field setPlayerJob fun(playerId: integer, jobName: string, jobGrade: integer): boolean
+---@field playerHasJob fun(playerId: integer, jobName: string, jobGrade: integer): boolean
