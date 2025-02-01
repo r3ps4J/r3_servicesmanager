597b5a211b0a5a92eec798bc7aefbdd08f1b8a85
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2025-02-01 21:05:06 +0100

    Create identity provider for ESX

---
+++ providers/identity/es_extended/sv_identity.lua
@@ -0,0 +1,27 @@
+RegisterOnResourceStart("identity", function()
+    local ESX = exports["es_extended"].getSharedObject()
+
+    ---@type ServerIdentityProvider
+    local identityProvider = {
+        getPlayerIdentifier = function(playerId)
+            local xPlayer = ESX.GetPlayerFromId(playerId)
+
+            if xPlayer == nil then
+                return nil
+            end
+
+            return xPlayer.getIdentifier()
+        end,
+        getPlayerName = function(playerId)
+            local xPlayer = ESX.GetPlayerFromId(playerId)
+
+            if xPlayer == nil then
+                return nil
+            end
+
+            return xPlayer.getName()
+        end,
+    }
+
+    return identityProvider
+end, ServicePriority.Lowest, "es_extended")
