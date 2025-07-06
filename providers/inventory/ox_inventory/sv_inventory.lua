7b614441c4851eb781c235f09df8bfedc8710abc
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2025-07-07 01:44:11 +0200

    Create inventory provider for ox_inventory

---
+++ providers/inventory/ox_inventory/sv_inventory.lua
@@ -0,0 +1,39 @@
+RegisterOnResourceStart("inventory", function()
+    ---@param inventoryRef InventoryRef
+    local function getInventory(inventoryRef)
+        if inventoryRef.type == "trunk" then
+            return "trunk" .. inventoryRef.id
+        elseif inventoryRef.type == "glovebox" then
+            return "glove" .. inventoryRef.id
+        end
+        return inventoryRef.id
+    end
+
+    ---@type ServerInventoryProvider
+    local inventoryProvider = {
+        addItem = function(inventoryRef, itemName, amount)
+            local inv = getInventory(inventoryRef)
+            local success, _response = exports.ox_inventory:AddItem(inv, itemName, amount)
+            return success
+        end,
+        removeItem = function(inventoryRef, itemName, amount)
+            local inv = getInventory(inventoryRef)
+            local success, _response = exports.ox_inventory:RemoveItem(inv, itemName, amount)
+            return success
+        end,
+        getItemCount = function(inventoryRef, itemName)
+            local inv = getInventory(inventoryRef)
+            return exports.ox_inventory:GetItemCount(inv, itemName)
+        end,
+        hasItem = function(inventoryRef, itemName, amount)
+            local inv = getInventory(inventoryRef)
+            return exports.ox_inventory:GetItemCount(inv, itemName) > amount
+        end,
+        canCarryItem = function(inventoryRef, itemName, amount)
+            local inv = getInventory(inventoryRef)
+            return exports.ox_inventory:CanCarryItem(inv, itemName, amount)
+        end,
+    }
+
+    return inventoryProvider
+end, ServicePriority.Low, "ox_inventory")
