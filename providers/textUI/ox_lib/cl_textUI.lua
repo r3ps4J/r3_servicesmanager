98c6ddd94bb5d85e20fa1c0370211a5f514103f1
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2025-07-07 16:49:33 +0200

    Create text UI provider for ox_lib

---
+++ providers/textUI/ox_lib/cl_textUI.lua
@@ -0,0 +1,13 @@
+RegisterOnResourceStart("textUI", function()
+    ---@type ClientTextUIProvider
+    local textUIProvider = {
+        showTextUI = function(text)
+            exports.ox_lib:showTextUI(text)
+        end,
+        hideTextUI = function()
+            exports.ox_lib:hideTextUI()
+        end,
+    }
+
+    return textUIProvider
+end, ServicePriority.Lowest, "ox_lib")
