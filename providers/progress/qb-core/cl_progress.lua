b3f7f4535482969b771996f35fa27709e3a50894
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2025-07-07 19:51:38 +0200

    Create progress provider for qb-core

---
+++ providers/progress/qb-core/cl_progress.lua
@@ -0,0 +1,56 @@
+RegisterOnResourceStart("progress", function()
+    local QBCore = exports["qb-core"]:GetCoreObject({ "Functions" })
+    local wasCancelled = false
+
+    ---@type ClientProgressProvider
+    local progressProvider = {
+        startProgress = function(label, duration, options)
+            wasCancelled = false
+            local animation = {}
+
+            if options.animation then
+                animation = {
+                    animDict = options.animation.dict,
+                    anim = options.animation.clip,
+                }
+            elseif options.scenario then
+                animation = {
+                    task = options.scenario,
+                }
+            end
+
+            QBCore.Functions.Progressbar(
+                "", -- Name is not actually used internally so we can hardcode it to ""
+                label,
+                duration,
+                false, -- useWhileDead
+                true, -- canCancel
+                {
+                    disableMovement = options.disable.movement,
+                    disableCarMovement = options.disable.vehicleMovement,
+                    disableMouse = options.disable.mouse,
+                    disableCombat = options.disable.combat,
+                },
+                animation,
+                {}, -- prop
+                {}, -- propTwo
+                function()
+                    -- Since we cannot cancel a progressbar in qbcore
+                    if wasCancelled then
+                        options.onCancel()
+                        return
+                    end
+                    options.onFinish()
+                end,
+                options.onCancel
+            )
+        end,
+        cancelProgress = function()
+            -- Since we cannot cancel a progressbar in qbcore
+            wasCancelled = true
+            TriggerEvent("progressbar:client:cancel")
+        end,
+    }
+
+    return progressProvider
+end, ServicePriority.Lowest, "qb-core")
