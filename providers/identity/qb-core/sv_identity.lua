6e6cdbc5233795fb567cf159b373d9d8331b6576
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2025-07-07 19:02:11 +0200

    Create identity provider for qb-core

---
+++ providers/identity/qb-core/sv_identity.lua
@@ -0,0 +1,25 @@
+RegisterOnResourceStart("identity", function()
+    local QBCore = exports["qb-core"]:GetCoreObject({ "Functions" })
+
+    ---@type ServerIdentityProvider
+    local identityProvider = {
+        getPlayerIdentifier = function(playerId)
+            local Player = QBCore.Functions.GetPlayer(playerId)
+            if Player == nil then
+                return nil
+            end
+
+            return Player.PlayerData.citizenid
+        end,
+        getPlayerName = function(playerId)
+            local Player = exports.qbx_core:GetPlayer(playerId)
+            if Player == nil then
+                return nil
+            end
+
+            return ("%s %s"):format(Player.PlayerData.charinfo.firstname, Player.PlayerData.charinfo.lastname)
+        end,
+    }
+
+    return identityProvider
+end, ServicePriority.Lowest, "qb-core")
