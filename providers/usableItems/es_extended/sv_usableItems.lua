33634746597ef4a6f20ab02d39f1160a07220cad
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2025-07-07 02:44:56 +0200

    Create usable items providers for qbox and esx

---
+++ providers/usableItems/es_extended/sv_usableItems.lua
@@ -0,0 +1,12 @@
+RegisterOnResourceStart("usableItems", function()
+    local ESX = exports["es_extended"].getSharedObject()
+
+    ---@type ServerUsableItemsProvider
+    local usableItemsProvider = {
+        registerUsableItem = function (itemName, cb)
+            ESX.RegisterUsableItem(itemName, cb)
+        end
+    }
+
+    return usableItemsProvider
+end, ServicePriority.Lowest, "es_extended")
---
+++ providers/usableItems/qbx_core/sv_usableItems.lua
@@ -0,0 +1,10 @@
+RegisterOnResourceStart("usableItems", function()
+    ---@type ServerUsableItemsProvider
+    local usableItemsProvider = {
+        registerUsableItem = function (itemName, cb)
+            exports.qbx_core:CreateUseableItem(itemName, cb)
+        end
+    }
+
+    return usableItemsProvider
+end, ServicePriority.Lowest, "qbx_core")
