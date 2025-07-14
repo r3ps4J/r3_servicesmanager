cf46c40aaeee5ea59c7b1a2f6bba26922f639b2e
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2025-07-14 14:05:55 +0200

    Add missing semicolon to identity.ts

--- api/identity/definitions/identity.ts
+++ api/identity/definitions/identity.ts
@@ -1,4 +1,4 @@
 export interface ServerIdentityProvider {
     getPlayerIdentifier(playerId: number): string | undefined;
-    getPlayerName(playerId: number): string
+    getPlayerName(playerId: number): string;
 }
