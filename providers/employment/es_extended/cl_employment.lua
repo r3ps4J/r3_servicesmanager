ce79e54dc1a4434e0d93207b7c079a66d282ac45
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2025-07-16 14:10:48 +0200

    Implement onPlayerJobChanged in esx client employment provider

--- providers/employment/es_extended/cl_employment.lua
+++ providers/employment/es_extended/cl_employment.lua
@@ -1,24 +1,28 @@
 RegisterOnResourceStart("employment", function()
     local ESX = exports["es_extended"]:getSharedObject()
-
     local bossGradeName = "boss"
+
+    local eventEmitter = EventEmitter()
+
+    local function convertJob(xPlayerJob)
+        return {
+            name = xPlayerJob.name,
+            label = xPlayerJob.label,
+            grade = {
+                rank = xPlayerJob.grade,
+                name = xPlayerJob.grade_name,
+                label = xPlayerJob.grade_label,
+                salary = xPlayerJob.grade_salary,
+                isBoss = xPlayerJob.grade_name == bossGradeName,
+            },
+        }
+    end
 
     ---@type ClientEmploymentProvider
     local employmentProvider = {
         getPlayerJob = function()
             local xPlayerJob = ESX.GetPlayerData().job
-
-            return {
-                name = xPlayerJob.name,
-                label = xPlayerJob.label,
-                grade = {
-                    rank = xPlayerJob.grade,
-                    name = xPlayerJob.grade_name,
-                    label = xPlayerJob.grade_label,
-                    salary = xPlayerJob.grade_salary,
-                    isBoss = xPlayerJob.grade_name == bossGradeName,
-                },
-            }
+            return convertJob(xPlayerJob)
         end,
         playerHasJob = function(jobName, jobGrade)
             local xPlayerJob = ESX.GetPlayerData().job
@@ -33,7 +37,14 @@ RegisterOnResourceStart("employment", function()
 
             return true
         end,
+        onPlayerJobChanged = function(cb)
+            eventEmitter.addEventListener("onJobChanged", cb)
+        end,
     }
+
+    AddEventHandler("esx:setJob", function(xPlayerJob)
+        eventEmitter.emit("onPlayerJobChanged", convertJob(xPlayerJob))
+    end)
 
     return employmentProvider
 end, ServicePriority.Lowest, "es_extended")
