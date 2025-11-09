692291203be933b6e61bdf6dbab61190826f5fd0
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2025-11-09 14:37:13 +0100

    Create menu provider for ox_lib

---
+++ providers/menu/ox_lib/cl_menu.lua
@@ -0,0 +1,29 @@
+RegisterOnResourceStart("menu", function()
+    ---@type ClientMenuProvider
+    local menuProvider = {
+        openMenu = function(options)
+            local elements = {}
+            for i, menuElement in ipairs(options.elements) do
+                elements[#elements + 1] = {
+                    title = menuElement.title,
+                    description = menuElement.description,
+                    icon = menuElement.icon,
+                    disabled = menuElement.disabled,
+                    onSelect = menuElement.onSelect,
+                }
+            end
+
+            exports.ox_lib:registerContext({
+                id = options.id,
+                title = options.title,
+                options = elements,
+            })
+            exports.ox_lib:showContext(options.id)
+        end,
+        closeMenu = function()
+            exports.ox_lib:hideContext()
+        end,
+    }
+
+    return menuProvider
+end, ServicePriority.Low, "ox_lib")
