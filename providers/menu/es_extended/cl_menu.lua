5339d271d70e3e02d6c3359fb9b26f6b6f8988f1
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2025-11-09 14:37:04 +0100

    Create menu provider for esx

---
+++ providers/menu/es_extended/cl_menu.lua
@@ -0,0 +1,33 @@
+RegisterOnResourceStart("menu", function()
+    local ESX = exports["es_extended"]:getSharedObject()
+
+    ---@type ClientMenuProvider
+    local menuProvider = {
+        openMenu = function(options)
+            local elements = {
+                {
+                    title = options.title,
+                    unselectable = true,
+                },
+            }
+            for i, menuElement in ipairs(options.elements) do
+                elements[#elements + 1] = {
+                    title = menuElement.title,
+                    description = menuElement.description,
+                    icon = menuElement.icon,
+                    disabled = menuElement.disabled,
+                    onSelect = menuElement.onSelect,
+                }
+            end
+
+            ESX.OpenContext("right", elements, function(menu, element)
+                if element.onSelect then
+                    element.onSelect()
+                end
+            end)
+        end,
+        closeMenu = ESX.CloseContext,
+    }
+
+    return menuProvider
+end, ServicePriority.Lowest, "es_extended")
