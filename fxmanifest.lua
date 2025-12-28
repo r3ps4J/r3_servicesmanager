0e55e4661e24e351d34154c80b4ae3e94a5391b2
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2025-12-28 21:46:18 +0100

    Bump version to 1.0.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ use_experimental_fxv2_oal "yes"
 
 -- Resource information
 name "r3_servicesmanager"
-version "0.3.0"
+version "1.0.0"
 description "Services manager for FXServer"
 author "r3ps4J"
 contact "discord.gg/bEWmBbg"
